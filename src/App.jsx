e0653a7c2f087db79ba142dc1922340992864cb6
joe <joe.chang1014@gmail.com>
2022-01-26 22:08:11 +0800

    feat 新增計分

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import { useState, useEffect } from 'react'
-import { CHESS_LIST, 
+import {
+  CHESS_LIST,
   checkStep,
   checkCanEat,
-  ROW, COL, combineChess, getColor, getLevel } from "./utils"
+  ROW, COL, combineChess, getColor, getLevel
+} from "./utils"
 import Chess from './component/Chess'
 import './App.css'
 import clsx from 'clsx'
 
 
 function App() {
-  const [count, setCount] = useState({B:0, R:0}) //紀錄被吃掉的棋子
+  const [count, setCount] = useState({ B: 0, R: 0 }) //紀錄被吃掉的棋子
   const [active, setActive] = useState(null) //紀錄被選中的格子
   const [turn, setTurn] = useState(1)//紀錄當前遊戲方
-  const [{ player1, player2 }, setPlayer] = useState({player1:null,player2:null}) //紀錄紅黑方
+  const [{ player1, player2 }, setPlayer] = useState({ player1: null, player2: null }) //紀錄紅黑方
   const [chess, setChess] = useState([]) //紀錄棋子位置
   const init = () => {
     setChess(combineChess())
+    setCount({ B: 0, R: 0 })
+    setTurn(1)
   }
   useEffect(() => {
     init()
@@ -23,64 +27,78 @@ function App() {
   }, []);
 
   useEffect(() => {
+    console.warn("chess", chess);
+  }, [chess]);
+
+  useEffect(() => {
     console.log(count);
-    if(count.B >= 16){
+    if (count.B >= 16) {
       alert("紅方勝利")
+      init()
     }
-    if(count.R >= 16){
+    if (count.R >= 16) {
       alert("黑方勝利")
+      init()
     }
   }, [count]);
 
-  const clickChess = ({id, type, isOpen}) => {
-      if(player1 === null){
-        decidePlayer(type)
-      }
+  const clickChess = ({ id, type, isOpen, index }) => {
+    if (player1 === null) {
+      decidePlayer(type)
+    }
 
-      //被點擊的棋子是否開啟
+    if (!isOpen) {
+      !active && showChess(index)
+      return
+    }
+
+    //被點擊的棋子是否開啟
 
 
-      if(active && !isOpen) return
-      let isSelf = checkSelf(type)
-      console.log("isSelf", isSelf);
-      console.log("active", active);
-      if(!active){
-        //檢查是否為己方棋子
-        if(!isSelf) return
-        console.log("setting!!!");
-        setActive({id, type})
-        return
-      }
+    // if(active && !isOpen) return
+    let isSelf = checkSelf(type)
+    if (!active) {
+      //檢查是否為己方棋子
+      if (!isSelf) return
+      console.log("setting!!!");
+      setActive({ id, type, isOpen, index })
+      return
+    }
 
-      //點擊已選取的格子
-      if(active.id === id){
-        setActive(null)
-        return
-      }
-      console.log("8777777", id);
-      //是否選擇第二個棋子
-      if(active.id !== id){
-        if(isSelf) return
-        const selfIndex = findChessIndex(active.id);
-        const targetIndex = findChessIndex(id);
-        const isBomb = getLevel(active.type) === 2;
-        //點到空白處
-        console.log(checkStep(selfIndex, targetIndex), "checkStep");
-        if(!checkStep(selfIndex, targetIndex, isBomb)) return
-        if(!id){
-          console.log("no-id");
+    //點擊已選取的格子
+    if (active.id === id) {
+      setActive(null)
+      return
+    }
+    console.log("8777777", id);
+    //是否選擇第二個棋子
+    if (active.id !== id) {
+      if (isSelf) return
+      // const selfIndex = findChessIndex(active.id);
+      // const targetIndex = index ?? findChessIndex(id);
+      const selfIndex = active.index;
+      const targetIndex = index;
+      const isBomb = getLevel(active.type) === 2;
+      //點到空白處
+      console.warn("selfIndex", selfIndex);
+      console.warn("targetIndex", targetIndex);
+      console.log(checkStep(selfIndex, targetIndex), "checkStep");
+      if (!checkStep(selfIndex, targetIndex, isBomb)) return
+      if (!id) {
+        console.log("no-id");
+        moveChess(selfIndex, targetIndex)
+      } else {
+        console.log("isBomb", isBomb);
+        console.log(checkCanEat(active.type, type), "checkCanEat");
+        if (checkCanEat(active.type, type)) {
           moveChess(selfIndex, targetIndex)
-        } else{
-          console.log("isBomb", isBomb);
-          console.log(checkCanEat(active.type, type), "checkCanEat");
-          if (checkCanEat(active.type, type)){
-            moveChess(selfIndex, targetIndex)
-          }
+          eatChess(type)
         }
-        setTurn(turn*-1)
-        setActive(null)
       }
-      
+      setTurn(turn * -1)
+      setActive(null)
+    }
+
   }
 
   const checkSelf = (type) => {
@@ -97,45 +115,54 @@ function App() {
   const moveChess = (selfIndex, targetIndex) => {
     let temp = [...chess];
     temp[targetIndex] = temp[selfIndex];
-    temp[selfIndex] = {id:"",type:""}
+    temp[selfIndex] = { id: "", type: "", isOpen: true, index:selfIndex }
     setChess(temp)
+    console.warn('temp[targetIndex]', temp[targetIndex]);
+    console.warn("temp[selfIndex]", temp[selfIndex]);
   }
 
-  
-  const showChess = (id) => {
+
+  const showChess = (index) => {
     const copy = [...chess]
-    const index = chess.findIndex(vo => vo.id === id )
-    copy[index] = { ...copy[index], isOpen:true }
+    copy[index] = { ...copy[index], isOpen: true }
     setChess(copy)
-    setTurn(turn*-1)
+    setTurn(turn * -1)
   }
 
   const decidePlayer = (type) => {
     const color = getColor(type)
-    console.log(type,33);
+    console.log(type, 33);
     color === "B" ?
-    setPlayer({player1:'B',player2:'R'}):
-    setPlayer({player1:'R',player2:'B'})
+      setPlayer({ player1: 'B', player2: 'R' }) :
+      setPlayer({ player1: 'R', player2: 'B' })
+  }
+
+  const eatChess = (type) => {
+    const color = getColor(type)
+    const temp = { ...count }
+    color === "B" && temp.B++
+    color === "R" && temp.R++
+    setCount(temp);
   }
 
   const transPlayer = (type) => {
-      if(!type) return ''
-      return type === "B" ? '黑方':'紅方'
+    if (!type) return ''
+    return type === "B" ? '黑方' : '紅方'
   }
 
   const transColor = (type) => {
-    if(!type) return ''
-    return type === "B" ? 'bg-black':'bg-red-600'
-  } 
+    if (!type) return ''
+    return type === "B" ? 'bg-black' : 'bg-red-600'
+  }
 
   return (
     <div className="App h-screen flex justify-center items-center">
       <ul className=" absolute top-8 text-xl">
-        <li>玩家1:{transPlayer(player1) }</li>
+        <li>玩家1:{transPlayer(player1)}</li>
         <li>玩家2:{transPlayer(player2)}</li>
         {
-          player1 !==0 && <li className='flex'>當前輪到 { turn === 1 ? '玩家1':'玩家2' }
-            <span className={clsx('dot', turn === 1 ? transColor(player1) : transColor(player2)) }></span>
+          player1 !== 0 && <li className='flex'>當前輪到 {turn === 1 ? '玩家1' : '玩家2'}
+            <span className={clsx('dot', turn === 1 ? transColor(player1) : transColor(player2))}></span>
           </li>
         }
       </ul>
@@ -145,7 +172,7 @@ function App() {
             return (
               <ul className='border-r line' key={'col' + i}>
                 {
-                  Array.from({ length: ROW }).map((item, j) =>  <Chess key={'row' + j} clickChess={clickChess} isActive={active?.id === chess[i * ROW + j].id} data={chess[i * ROW + j]} />)
+                  Array.from({ length: ROW }).map((item, j) => <Chess key={'row' + j} clickChess={clickChess} isActive={active?.id === chess[i * ROW + j].id} index={i * ROW + j} data={chess[i * ROW + j]} />)
                 }
               </ul>
             )
--- src/component/Chess.jsx
+++ src/component/Chess.jsx
@@ -1,28 +1,27 @@
 import React from 'react';
 import clsx from 'clsx';
 import { CHESS_LIST } from "../utils"
-function Chess({ data:{id, type, isOpen}, clickChess, isActive}) {
+function Chess({ data: { id, type, isOpen }, index, clickChess, isActive }) {
     const handleClick = () => {
-        console.log("id", id);
         const sendData = {
             id,
             type,
-            isOpen
+            isOpen,
+            index
         }
         clickChess(sendData)
     }
     const isRed = type.substr(0, 1) === "R"
-    
-    return <li className={clsx("box", isActive && 'bg-yellow-200') } onClick={handleClick}>
+    return <li className={clsx("box", isActive && 'bg-yellow-200')} onClick={handleClick}>
         {
             id ?
-            isOpen ? 
-            <span className={clsx("chess", isRed && 'text-red-700 border-red-700')}> 
-                {CHESS_LIST[type]} 
-            </span>:
-            <span className={clsx("chess", "back")}></span> :null
+                isOpen ?
+                    <span className={clsx("chess", isRed && 'text-red-700 border-red-700')}>
+                        {CHESS_LIST[type]}
+                    </span> :
+                    <span className={clsx("chess", "back")}></span> : null
         }
-        
+
     </li>
 }
 
